c4be4e43010f0dee6cc4d606bd152dc1c079b97e
Piotr Witkiewicz <piotr.witkiewicz.stud@pw.edu.pl>
2021-12-04 13:30:27 +0100

    day4 done

---
+++ src/main/java/aoc/day4/Board.java
@@ -0,0 +1,60 @@
+package aoc.day4;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class Board {
+    private List<BoardRow> rows;
+    private boolean won;
+
+    public static Board of(final List<String> input) {
+        Board board = new Board();
+        board.setRows(new ArrayList<>());
+        board.won = false;
+
+        var verticalRows = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < input.size(); i++) {
+            verticalRows.add(new ArrayList<>());
+        }
+
+        for (String s : input) {
+            var list = Arrays.stream(s.trim().split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            board.getRows().add(BoardRow.of(list));
+            for (int j = 0; j < list.size(); j++) {
+                verticalRows.get(j).add(list.get(j));
+            }
+        }
+
+        for (var row : verticalRows) {
+            board.getRows().add(BoardRow.of(row));
+        }
+
+        return board;
+    }
+
+    public boolean markNumber(final int number) {
+        var anyMarked = false;
+        for (BoardRow row : rows) {
+            if (row.markNumber(number)) {
+                anyMarked = true;
+            }
+        }
+        return anyMarked;
+    }
+
+    public int getScore() {
+        return rows.stream()
+                .map(BoardRow::getUnmarkedNumbers)
+                .flatMap(Collection::stream)
+                .reduce(0, Integer::sum) / 2;
+    }
+}
---
+++ src/main/java/aoc/day4/Day4.java
@@ -0,0 +1,49 @@
+package aoc.day4;
+
+import aoc.utility.Reader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day4 {
+    private static List<Integer> parseDrawnNumbers(final String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Board> parseBoards(final List<String> input) {
+        final var boardsList = new ArrayList<Board>();
+
+        for (int i = 6; i < input.size(); i += 6) {
+            boardsList.add(Board.of(input.subList(i - 4, i + 1)));
+        }
+
+        return boardsList;
+    }
+
+    private static void playBingo(final List<Integer> drawnNumbers, final List<Board> boardsList) {
+        for (int number : drawnNumbers) {
+            for (int i = 0; i < boardsList.size(); i++) {
+                var currentBoard = boardsList.get(i);
+
+                if (!currentBoard.isWon() && currentBoard.markNumber(number)) {
+                    var score = boardsList.get(i).getScore();
+                    var string = String.format("winning board %d with score %d", i+1, score * number);
+                    System.out.println(string);
+                    currentBoard.setWon(true);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        var input = Reader.readLinesAsStrings("input");
+        var drawnNumbers = parseDrawnNumbers(input.get(0));
+        var boardsList = parseBoards(input);
+
+        playBingo(drawnNumbers, boardsList);
+    }
+}
